47931b70f8018123ae986a182c8bdd87f5de3667
Hari2920 <113305898+Hari2920@users.noreply.github.com>
2022-12-01 11:01:53 +0530

    Add files via upload

---
+++ activatorApplication/src/main/java/com/gl/activatorApplication/ActivatorApplication.java
@@ -0,0 +1,13 @@
+package com.gl.activatorApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivatorApplication.class, args);
+	}
+
+}
